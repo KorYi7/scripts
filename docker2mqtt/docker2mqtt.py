ee3b3cf971902c3e289d69335050bad202b20881
KorYi7 <61246985+KorYi7@users.noreply.github.com>
2020-03-22 17:54:43 +0000

    V1.0

--- docker2mqtt/docker2mqtt.py
+++ docker2mqtt/docker2mqtt.py
@@ -2,27 +2,28 @@ import paho.mqtt.client as mqtt
 import subprocess
 import time
 from subprocess import PIPE
+
 from secret import mqttuser
 from secret import mqttpass
 from secret import mqtturl
 from secret import dockerssh
 
-# client = mqtt.Client("docker")
+client = mqtt.Client("docker")
 
-# client.username_pw_set(mqttuser,mqttpass)
-# client.connect(mqtturl)
-# client.publish("test","ON")
+client.username_pw_set(mqttuser,mqttpass)
+client.connect(mqtturl)
 
 containers = ["tsukihi"]
 states = [""]
+payload = "{"
+
 while True:
 	output=subprocess.run(["ssh", dockerssh, '''docker ps -a --format "{{.Names}};{{.Status}}"'''],stdout=PIPE,stderr=PIPE)
 	# output=subprocess.run(["ssh", dockerssh, '''docker ps -a --format "{{.Names}};{{.Status}}"'''],capture_output=True)
 	if output.returncode == 0:
 		if states[0]!="Up":
 			states[0]="Up"
-			# client.publish(containers[0],states[0])
-			print(containers[0],states[0])
+			payload+='"'+containers[0]+'":"Up"'
 		for line in output.stdout.decode("utf-8").splitlines():
 			name, rawstate = line.split(";",1)
 			state = rawstate.split(" ",1)[0]
@@ -33,16 +34,18 @@ while True:
 			except ValueError:
 				containers.append(name)
 				states.append(state)
-				# client.publish(name,state)
-				print(name,state)
+				payload+='"'+name+'":"'+state+'",'
 			else:
 				if states[index]!=state:
 					states[index]=state
-					# client.publish(name,state)
-					print(name,state)
+					payload+='"'+name+'":"'+state+'",'
 	else:
 		if states[0]=="Up":
-			states[0]="Down"
-			# client.publish(containers[0],states[0])
-			print(containers[0],states[0])
-	time.sleep(10)
+			for i in range(len(states)):
+				payload+='"'+containers[i]+'":"Down",'
+				states[i]="Down"
+	if payload!="{":
+		client.publish("docker2mqtt/StateChanged",payload+"}")
+		# print(payload[:-1]+"}")
+		payload="{"
+	time.sleep(60)
